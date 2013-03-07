80a8e0743f4168a0ddd6d0c14260da5c1b6c4b83
spongik <isa820@mail.ru>
2013-03-08 01:15:42 +0400

    видео в пабе открывается в окне 800х600

--- src/knbPatch.js
+++ src/knbPatch.js
@@ -26,15 +26,16 @@ $(function() {
 		url = $('#comments .showMore').data('url').replace(/max_id=\d+&/g, "max_id=9999999999&");		
 		$('#comments .showMore').attr('data-url', url);
 		$('#comments .comment').attr('old', '1');
-		$.fn.watch(function(clean) {
+		var onRefresh = function(clean) {
 			$comments = $('#comments .comment[old!=1]');
 			if  ($comments.length > 0) {
-				clean && clean();
+				$('#comments').unbind('DOMSubtreeModified', onRefresh);
 				$('#comments .comment[old=1]').remove();
 				document.location.replace('#refresh-comments');
 			}
-		});
+		};
 		$('#comments .showMore, #comments .showMore .wrapBtnTxt').trigger('click');
+		$('#comments').bind('DOMSubtreeModified', onRefresh);
 		
 		ev.preventDefault();
 		return false;
@@ -43,10 +44,9 @@ $(function() {
 	$refresh.wrap('<div id="refresh-comments-wrap"></div>');
 	
 	// comments highlight
-	var bindComments = function(clean) {
+	var bindComments = function() {
 		$comments = $('#comments .comment[binded!=1]');
 		if  ($comments.length > 0) {
-			clean && clean();
 			$comments
 				.attr('binded', '1')
 					.hover(function (ev) {
@@ -65,10 +65,77 @@ $(function() {
 				});
 		}
 	};
-	$('#comments .showMore').click(function() {
-		$.fn.watch(bindComments);
-	});
+	$('#comments').bind('DOMSubtreeModified', bindComments);
 	bindComments();
+	
+	// play video	
+	var onVideoPlay = function (ev) {
+		$this = $('.embedVideo', ev.data.element);
+		$iframe = $(">iframe", $this);
+		if ($this.length > 0 && $iframe.length > 0) {
+			$this.unbind('DOMSubtreeModified', onVideoPlay);
+			$this
+				.width($iframe.width())
+				.height($iframe.height());
+			$iframeClone = $iframe.clone().css('display', 'none');
+			background = $("img", $this).attr('src');
+			$this.html('<div class="playBtn"></div>')
+				.css('background', 'url(' + background + ')')
+				.css('background-size:', 'contain');
+			id = 'video-iframe-' + ev.data.element.data('object-id');
+			$iframeClone.attr('id', id);
+			$('#back').append($iframeClone);
+			
+			$('.embedVideo', ev.data.element).bind('click', {id: id} , function (ev2) {
+				fId = ev2.data.id;
+				w = $(window).width();
+				h = $(window).height();			
+				fw = 800;
+				fh = 600;
+				$('#back').show();
+				$('#' + fId)
+					.css('position', 'fixed')
+					.width(fw)
+					.height(fh)
+					.css('left', Math.round((w - fw) / 2) + 'px')
+					.css('top', Math.round((h - fh) / 2) + 'px')
+					.css('display', 'block');
+				ev2.preventDefault();
+				ev2.stopPropagation();
+			}).trigger('click');
+		}
+	};
+	var onPostsLoaded = function (ev) {
+		$this = $(this);
+		$posts = $(">li[binded!=1]", $this);
+		if  ($posts.length > 0) {
+			$posts
+				.attr('binded', '1')
+				.each(function (i, el) {
+					$el = $(el);
+					$('.embedVideo', $el).bind('DOMSubtreeModified', {element: $el}, onVideoPlay);
+				});
+		}
+	};
+	$('.postListBlock .postList').bind('DOMSubtreeModified', onPostsLoaded);
+	
+	$back = $('<div></div>')
+		.attr('id', 'back')
+		.css('display', 'none')
+		.css('position', 'fixed')
+		.css('width', '100%')
+		.css('height', '100%')
+		.css('left', '0')
+		.css('top', '0')
+		.css('background', 'rgba(0,0,0,0.9)')
+		.css('z-index', '99999')
+		.bind('click', function (ev) {
+			$this = $(this);
+			$this.find('iframe').css('display', 'none');
+			$this.fadeOut();
+			ev.preventDefault();
+		});
+	$('body').append($back);
 	
 	// Вкладка "всё"
 	$('.tabs').find('ul').append('<li><a href="#" class="allShowLink" data-grid="4"><i></i><span>Все</span><b></b></a></li>');
@@ -84,7 +151,7 @@ $(function() {
 		},2000);
 		}else{
 			$('#gridScroll, #gridWrapper').animate({
-			height:517
+				height:517
 			},500);		
 		}
 	});
