8a655fc54ab9e00e4374e3fc1429f4ded6721fe8
spongik <isa820@mail.ru>
2013-03-08 23:09:50 +0400

    фильтр постов на странице профиля

--- src/scripts/plugins/pub-filter.js
+++ src/scripts/plugins/pub-filter.js
@@ -2,7 +2,7 @@ $(function() {
 	
 	var PUB_FILTER_KEY = 'pub-filter-threshold';
 	
-	$filter = $('<ul id="threshold-filter" class="filter2" style="margin-right: 20px;">\
+	$filter = $('<ul id="threshold-filter" class="filter2">\
 			<li><a data-threshold="-1" href="#">Все</a></li>\
 			<li><a data-threshold="5" href="#">&gt; 5</a></li>\
 			<li><a data-threshold="20" href="#">&gt; 20</a></li>\
@@ -16,10 +16,6 @@ $(function() {
 	}
 
 	var filterPosts = function() {
-		if (threshold < 0) {
-			return;
-		}
-		
 		reloadTimeout && clearTimeout(reloadTimeout);
 		
 		$columns = $('.postListBlock > .postList');
@@ -35,19 +31,22 @@ $(function() {
 						$post.show();
 					}
 				});
-			$columnMax = $columns.first().height() >  $columns.last().height() ? $columns.first() : $columns.last();
-			$columnMin = $columns.first().height() <= $columns.last().height() ? $columns.first() : $columns.last();
 			
-			for (;;) {
-				$last = $('.userMessageBlock[moved!=1][data-object-id]:visible', $columnMax).last();
-				if ($last.length > 0 && $columnMax.height() - $last.height() - $columnMin.height() > 0) {
-					$columnMin.append($last.attr('moved', '1'));
-				} else {
-					break;
+			if (threshold >= 0) {		
+				$columnMax = $columns.first().height() >  $columns.last().height() ? $columns.first() : $columns.last();
+				$columnMin = $columns.first().height() <= $columns.last().height() ? $columns.first() : $columns.last();
+				
+				for (;;) {
+					$last = $('.userMessageBlock[moved!=1][data-object-id]:visible', $columnMax).last();
+					if ($last.length > 0 && $columnMax.height() - $last.height() - $columnMin.height() > 0) {
+						$columnMin.append($last.attr('moved', '1'));
+					} else {
+						break;
+					}
 				}
+				$('.userMessageBlock[moved!=1]', $columnMax).attr('moved', '1');
+				$('.userMessageBlock[moved!=1]', $columnMin).attr('moved', '1');
 			}
-			$('.userMessageBlock[moved!=1]', $columnMax).attr('moved', '1');
-			$('.userMessageBlock[moved!=1]', $columnMin).attr('moved', '1');
 		}
 		reloadTimeout = setTimeout(function() {
 			$(window).scrollTop( $(window).scrollTop() + 1 );
@@ -74,6 +73,10 @@ $(function() {
 		}
 	});
 	
-	$filter.insertAfter('.headFilter .filter');
+	if (document.location.href.indexOf('http://kanobu.ru/accounts/') == 0) {
+		$filter.css('top', '40px').css('position', 'relative').insertBefore('.content h2');
+	} else {
+		$filter.css('margin-right', '20px').insertAfter('.headFilter .filter');
+	}
 
 });
