59e15fc718bf49f400e3a23597959fa58d590349
spongik <isa820@mail.ru>
2013-03-08 15:05:46 +0400

    "Обновить комментарии" теперь на всех страница�

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -7,4 +7,14 @@ function exec(fn) {
 }
 
 exec(function () {
+	jQuery.ajaxSetup({
+		dataFilter: function(data, type) {
+			var json = $.parseJSON(data);
+			if (json.hasOwnProperty('list') && json.hasOwnProperty('more') && !json.more) {
+				json.more = '<a class="showMore showMoreFake" style="display: none;"><i></i><div class="wrapBtnTxt"></div><em></em></a>';
+			}
+			data = JSON.stringify(json);
+			return data;
+		},
+    });
 });
--- src/scripts/plugins/comments-refresh.js
+++ src/scripts/plugins/comments-refresh.js
@@ -1,13 +1,17 @@
 $(function() {
 
-	// refresh comments
 	var $refresh = $('<a href="#" id="refresh-comments" class="wrapBtnTxt">Обновить комментарии</a>');
+	var refreshUrl = null;
 	$refresh.click(function(ev) {
-		url = $('#comments .showMore').data('url').replace(/max_id=\d+&/g, "max_id=9999999999&");		
-		$('#comments .showMore').attr('data-url', url);
+		$showMore = $('#comments > .showMore');
+		if (refreshUrl == null) {
+			refreshUrl = $showMore.data('url').replace(/max_id=\d+&/g, "max_id=9999999999&");
+		}
+		$showMore.attr('data-url', refreshUrl);
 		$('#comments .comment').attr('old', '1');
+		
 		var lastId = null;
-		var onRefresh = function(clean) {
+		var onRefresh = function() {
 			$comments = $('#comments .comment[old!=1]');
 			if  ($comments.length > 0) {
 				$('#comments').unbind('DOMSubtreeModified', onRefresh);
@@ -26,13 +30,31 @@ $(function() {
 				lastId = null;
 			}
 		};
-		$('#comments .showMore, #comments .showMore .wrapBtnTxt').trigger('click');
 		$('#comments').bind('DOMSubtreeModified', onRefresh);
+		$('.wrapBtnTxt', $showMore).trigger('click');
 		
 		ev.preventDefault();
 		return false;
 	});
-	$('#comments .showMore').length > 0 && $refresh.insertBefore($('.writeComment'));
+	$('#comments > .showMore').length > 0 && $refresh.insertBefore($('.writeComment'));
 	$refresh.wrap('<div id="refresh-comments-wrap"></div>');
+
+	var tryCreateShowMore = function() {
+		$('body').unbind('DOMSubtreeModified', tryCreateShowMore);
+		if ($('#comments > .showMore').length == 0) {
+			data = $('.mainColumn > script:last').html();
+			objectId = /object_id: (\d+)/.exec(data)[1];
+            contentTypeId = /content_type: (\d+)/.exec(data)[1];
+			
+			$showMore = $('<a class="showMore"><i></i><div class="wrapBtnTxt"></div><em></em></a>');
+			$showMore.css('display', 'none');
+			$showMore.data('url', '/comments/list/' + contentTypeId + '/' + objectId + '/?max_id=000&paginate_by=20');
+			
+			$('#comments').append($showMore);
+			$refresh.insertBefore($('.writeComment'));
+			$refresh.wrap('<div id="refresh-comments-wrap"></div>');
+		}
+	};
+	$('body').bind('DOMSubtreeModified', tryCreateShowMore);
 
 });
