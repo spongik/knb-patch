3d70a87c866b23e6ebcd872723a8e0c7098a362a
spongik <isa820@mail.ru>
2013-03-08 15:10:22 +0400

    починил не json запросы

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -9,11 +9,13 @@ function exec(fn) {
 exec(function () {
 	jQuery.ajaxSetup({
 		dataFilter: function(data, type) {
-			var json = $.parseJSON(data);
-			if (json.hasOwnProperty('list') && json.hasOwnProperty('more') && !json.more) {
-				json.more = '<a class="showMore showMoreFake" style="display: none;"><i></i><div class="wrapBtnTxt"></div><em></em></a>';
+			if (data[0] == '{') {
+				var json = $.parseJSON(data);
+				if (json.hasOwnProperty('list') && json.hasOwnProperty('more') && !json.more) {
+					json.more = '<a class="showMore showMoreFake" style="display: none;"><i></i><div class="wrapBtnTxt"></div><em></em></a>';
+				}
+				data = JSON.stringify(json);
 			}
-			data = JSON.stringify(json);
 			return data;
 		},
     });
