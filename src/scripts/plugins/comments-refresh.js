759011ef8ac500fbb21aeeb53d5059d4f3e55690
spongik <isa820@mail.ru>
2013-03-09 15:19:10 +0400

    sticky кнопка "Обновить комментарии"

--- src/scripts/plugins/comments-refresh.js
+++ src/scripts/plugins/comments-refresh.js
@@ -1,6 +1,17 @@
 $(function() {
 
-	var $refresh = $('<a href="#" id="refresh-comments" class="wrapBtnTxt">Обновить комментарии</a>');
+	var updateRefreshBtnLabel = function(newCommentsCount) {
+		var refreshLabel = 'Обновить комментарии';
+		if (newCommentsCount) {
+			refreshLabel += ' (' + newCommentsCount + ')';
+		}
+		
+		$('#refresh-comments').html(refreshLabel);
+		$('#refresh-comments-float').html(refreshLabel);
+	};
+
+	var $refresh = $('<a href="#" id="refresh-comments" class="wrapBtnTxt">asd</a>');
+	var $floatRefresh = $('<a href="#" id="refresh-comments-float">asd</a>');
 	var refreshUrl = null;
 	$refresh.click(function(ev) {
 		$showMore = $('#comments > .showMore');
@@ -15,9 +26,12 @@ $(function() {
 			$comments = $('#comments .comment[old!=1]');
 			if  ($comments.length > 0) {
 				$('#comments').unbind('DOMSubtreeModified', onRefresh);
+				updateRefreshBtnLabel();
+				
 				$old = $('#comments .comment[old=1]');
 				lastId =  $old.last().data('id');
 				$old.remove();
+				
 				document.location.replace('#refresh-comments');
 			}
 			if (lastId != null) {
@@ -36,8 +50,29 @@ $(function() {
 		ev.preventDefault();
 		return false;
 	});
-	$('#comments > .showMore').length > 0 && $refresh.insertBefore($('.writeComment'));
-	$refresh.wrap('<div id="refresh-comments-wrap"></div>');
+	
+	$floatRefresh.click(function (ev) {
+		$.scrollTo( '#refresh-comments', 800, { onAfter: function() {
+			$refresh.trigger('click');
+		}});
+		
+		ev.preventDefault();
+		return false;
+	});
+	
+	var appendRefreshBtn = function() {
+		$refresh.insertBefore($('.writeComment'));
+		$refresh.wrap('<div id="refresh-comments-wrap"></div>');
+		
+		$('.rightColumn').append($floatRefresh);
+		$('#refresh-comments-float').sticky({topSpacing: 90});
+		
+		updateRefreshBtnLabel();
+	};
+	
+	if ($('#comments > .showMore').length > 0) {
+		appendRefreshBtn();
+	}
 
 	var tryCreateShowMore = function() {
 		$('body').unbind('DOMSubtreeModified', tryCreateShowMore);
@@ -51,8 +86,7 @@ $(function() {
 			$showMore.data('url', '/comments/list/' + contentTypeId + '/' + objectId + '/?max_id=000&paginate_by=20');
 			
 			$('#comments').append($showMore);
-			$refresh.insertBefore($('.writeComment'));
-			$refresh.wrap('<div id="refresh-comments-wrap"></div>');
+			appendRefreshBtn();
 		}
 	};
 	$('body').bind('DOMSubtreeModified', tryCreateShowMore);
