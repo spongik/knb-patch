5e389bd521bdd5ebe6430f6c3efc9292b2e0d215
spongik <isa820@mail.ru>
2013-03-09 17:43:05 +0400

    динамическая подсветка друзей

--- src/scripts/plugins/friends-highlight.js
+++ src/scripts/plugins/friends-highlight.js
@@ -18,15 +18,56 @@ $(function() {
 	}
 	
 	friends = $.parseJSON(localStorage.getItem(FRIENDS_HIGHLIGHT_KEY));
-	$('a.ava').each(function(i, user) {
-		$user = $(user);
-		profile = $user.attr('href');
-		for (var friend in friends) {
-			if (profile == friends[friend].profile) {
-				$user.addClass('friend');
-				break;
+	
+	friendsHighlight = function($users, cssClass) {
+		$users.each(function(i, user) {
+			$user = $(user);
+			profile = $user.attr('href');
+			for (var friend in friends) {
+				if (profile == friends[friend].profile) {
+					$user.addClass(cssClass);
+					break;
+				}
 			}
-		}
-	});
+		});
+	}
+	
+	// comments
+	commentsUpdated = function() {
+		$this = $(this);
+		$this.unbind('DOMSubtreeModified', commentsUpdated);
+		friendsHighlight($('.ava[friend!=1]', $(this)).attr('friend', '1').parent(), 'friendHighlight');
+		$this.bind('DOMSubtreeModified', commentsUpdated);
+	};
+	$('#comments').bind('DOMSubtreeModified', commentsUpdated);
+	
+	// pub posts
+	postListUpdated = function() {
+		$this = $(this);
+		$this.unbind('DOMSubtreeModified', postListUpdated);
+		friendsHighlight($('> li .ava[friend!=1]', $(this)).attr('friend', '1'), 'friendHighlight');
+		$this.bind('DOMSubtreeModified', postListUpdated);
+	}
+	$('.postList').bind('DOMSubtreeModified', postListUpdated);
+	
+	// shouts	
+	var isShoutsPage = document.location.href.indexOf('http://kanobu.ru/shouts/') == 0;
+	shoutsListUpdated = function() {
+		$this = $(this);
+		$this.unbind('DOMSubtreeModified', shoutsListUpdated);
+		$shouts = $('> li > .shout[friend!=1]', $(this)).attr('friend', '1');	
+		isShoutsPage && $shouts.each(function (i, shout) {
+			$(shout).parent().bind('DOMSubtreeModified', function() {
+				friendsHighlight($('.shoutAnswers .ava[friend!=1]', $(this)).attr('friend', '1'), 'friendHighlight');
+			});
+		});
+		friendsHighlight($('> li > .shout > .ava', $shouts), 'friendHighlight');
+		$this.bind('DOMSubtreeModified', shoutsListUpdated);
+	}
+	$('.shoutsList').bind('DOMSubtreeModified', shoutsListUpdated).trigger('DOMSubtreeModified');
+	
+	friendsHighlight($('.people > ul > li > a'), 'friendHighlight2'); // posts likes
+	friendsHighlight($('.rightColumn .author'), 'friendHighlight'); // post author
+	friendsHighlight($('.userMessageBlock .ava'), 'friendHighlight'); // pub post author
 
 });
