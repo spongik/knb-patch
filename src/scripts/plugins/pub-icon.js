905f33ea4cedf09e8fa4fe561ac1644809c50fae
spongik <isa820@mail.ru>
2013-03-09 12:45:28 +0400

    добавил кнопки паба в верхнем меню

---
+++ src/scripts/plugins/pub-icon.js
@@ -0,0 +1,6 @@
+$(function() {
+
+	$('.header > .hBtns').append($('<li class="pub"><a href="/pub/all/"><i></i>Все</a></li>'));
+	$('.header > .hBtns').append($('<li class="pub"><a href="/pub/recommended/"><i></i>Лучшее</a></li>'));
+
+});
