91a0553b061edb5993092b4937d35d266b775905
spongik <isa820@mail.ru>
2013-03-10 00:18:12 +0400

    форматирование кода

--- src/scripts/plugins/tab-all.js
+++ src/scripts/plugins/tab-all.js
@@ -1,21 +1,22 @@
 $(function() {
 
-	// Вкладка "всё"
-	$('.tabs').find('ul').append('<li><a href="#" class="allShowLink" data-grid="4"><i></i><span>Все</span><b></b></a></li>');
-	$('#gridScroll').append('<div class="matrixContainer allShow" data-grid-item="4"></div>')
-					.css('width','3840px');
-	$('.matrixContainer.allShow').append('<div class="matrixContainer">'+$('.matrixContainer[data-grid-item=1]').html()+'</div>')
-								.append('<div class="matrixContainer">'+$('.matrixContainer[data-grid-item=2]').html()+'</div>')
-								.append('<div class="matrixContainer">'+$('.matrixContainer[data-grid-item=3]').html()+'</div>');
-	$('.tabs a').click(function(){
-		if($(this).hasClass('allShowLink')){
-		$('#gridScroll, #gridWrapper').animate({
-			height:1551
-		},2000);
-		}else{
-			$('#gridScroll, #gridWrapper').animate({
-				height:517
-			},500);		
+	$('.tabs').find('ul')
+		.append('<li><a href="#" class="allShowLink" data-grid="4"><i></i><span>Все</span><b></b></a></li>');
+	
+	$('#gridScroll')
+		.append('<div class="matrixContainer allShow" data-grid-item="4"></div>')
+		.css('width','3840px');
+	
+	$('.matrixContainer.allShow')
+		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=1]').clone(true)))
+		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=2]').clone(true)))
+		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=3]').clone(true)));
+	
+	$('.tabs a').click(function() {
+		if ($(this).hasClass('allShowLink')) {
+			$('#gridScroll, #gridWrapper').height(1551);
+		} else {
+			$('#gridScroll, #gridWrapper').height(500);
 		}
 	});
 
