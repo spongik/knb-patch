50c4174c71c4fed286f0e0c39dc6d8857172d6af
spongik <isa820@mail.ru>
2013-03-10 17:42:51 +0400

    переименовал Лушее в Интересное

--- src/scripts/plugins/header-icons.js
+++ src/scripts/plugins/header-icons.js
@@ -6,7 +6,7 @@ $.knb.plugins.headerIcons = function () {
 	$('.header > .hBtns').append($('<li class="articles last"><a href="/articles/users/"><i></i>Юзеры</a></li>'));
 	
 	$('.header > .hBtns').append($('<li class="pub"><a href="/pub/all/"><i></i>Все</a></li>'));
-	$('.header > .hBtns').append($('<li class="pub"><a href="/pub/recommended/"><i></i>Лучшее</a></li>'));
+	$('.header > .hBtns').append($('<li class="pub long"><a href="/pub/recommended/"><i></i>Интересное</a></li>'));
 	$('.header > .hBtns').append($('<li class="pub"><a href="/pub/friends/"><i></i>Друзья</a></li>'));
 
 };
