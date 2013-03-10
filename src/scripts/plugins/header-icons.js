7d8b1a2ffe59ec0c5f8a42ffeed6c359c026ed1d
spongik <isa820@mail.ru>
2013-03-10 18:21:02 +0400

    кнопка epic

--- src/scripts/plugins/header-icons.js
+++ src/scripts/plugins/header-icons.js
@@ -2,6 +2,9 @@ $.knb.plugins.headerIcons = function () {
 
 	$('body').addClass('headerIcons');
 
+	$('.header > .hBtns').append($('<li class="epic"><a target="_blank" href="http://epic.kanobu.ru/" title="EPIC Канобу"><i></i></a></li>'));
+	$('.header > .hBtns .epic a').css('background', 'url("' + chrome.extension.getURL('images/epic.png') + '") no-repeat 12px 10px')
+	
 	$('.header > .hBtns').append($('<li class="articles"><a href="/articles/"><i></i>Редакция</a></li>'));
 	$('.header > .hBtns').append($('<li class="articles last"><a href="/articles/users/"><i></i>Юзеры</a></li>'));
 	
