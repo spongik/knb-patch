9ea366ee6a3cb8e0ddf786794de459f85a607122
spongik <isa820@mail.ru>
2013-03-11 22:00:50 +0400

    deploy-скрипт

---
+++ deploy/deploy.bat
@@ -0,0 +1,1 @@
+C:/Python32/python ./deploy.py ../src
---
+++ deploy/deploy.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import shutil
+import json
+import subprocess
+
+from distutils import dir_util
+
+if len(sys.argv) == 1:
+    sys.exit('Usage: %s [path to manifest.json]' % sys.argv[0])
+
+path = sys.argv[1]
+manifest_orig = path + '/manifest.json'; 
+ 
+if not os.path.exists(manifest_orig):
+    sys.exit('Error: manifest file "%s" not exists.' % manifest_orig)
+
+deploy_path = '.deploy'
+dir_util.copy_tree(path, deploy_path)
+
+manifest_path = deploy_path + '/manifest.json'; 
+manifest_raw = open(manifest_path, encoding='utf-8').read()
+manifest = json.loads(manifest_raw)
+
+i = 1
+for content_scripts_part in manifest['content_scripts']:
+    merged_js_path = 'scripts/merged.' + str(++i) + '.js';
+    with open(deploy_path + '/' + merged_js_path, encoding='utf-8', mode='w+') as merged_js:
+        scripts = content_scripts_part['js']
+        for script in scripts:
+            script_path = deploy_path + '/' + script;
+            merged_js.write(open(script_path, encoding='utf-8').read() + '\r\n\r\n')
+            os.unlink(script_path)
+        merged_js.close()
+        merged_js_full_path = deploy_path + '/' + merged_js_path
+        subprocess.call(['tools/AjaxMin.exe', merged_js_full_path, '-out', merged_js_full_path, '–clobber'])
+        content_scripts_part['js'] = [merged_js_path]
+
+with open(deploy_path + '/manifest.json', encoding='utf-8', mode='w+') as manifest_merged:
+    json.dump(manifest, manifest_merged)
