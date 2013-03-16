52bbfd32080db61a539e81f08c213d4e60a3a877
spongik <isa820@mail.ru>
2013-03-16 20:06:22 +0400

    отключил асинхронную загрузку для вкладки Все

--- src/scripts/plugins/tab-all.js
+++ src/scripts/plugins/tab-all.js
@@ -1,26 +1,28 @@
 $.knb.plugins.tabAll = {};
 
-$.knb.plugins.tabAll.async = true;
+$.knb.plugins.tabAll.async = false;
 $.knb.plugins.tabAll.run = function () {
 
-	$('.tabs').find('ul')
-		.append('<li><a href="#" class="allShowLink" data-grid="4"><i></i><span>Все</span><b></b></a></li>');
-	
-	$('#gridScroll')
-		.append('<div class="matrixContainer allShow" data-grid-item="4"></div>')
-		.css('width','3840px');
-	
-	$('.matrixContainer.allShow')
-		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=3]').clone(true)))
-		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=2]').clone(true)))
-		.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=1]').clone(true)));
-	
-	$('.tabs a').click(function() {
-		if ($(this).hasClass('allShowLink')) {
-			$('#gridScroll, #gridWrapper').height(1551);
-		} else {
-			$('#gridScroll, #gridWrapper').height(500);
-		}
-	});
+	if ($('.tabs').length > 0) {
+		$('.tabs').find('ul')
+			.append('<li><a href="#" class="allShowLink" data-grid="4"><i></i><span>Все</span><b></b></a></li>');
+		
+		$('#gridScroll')
+			.append('<div class="matrixContainer allShow" data-grid-item="4"></div>')
+			.css('width','3840px');
+		
+		$('.matrixContainer.allShow')
+			.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=3]').clone(true)))
+			.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=2]').clone(true)))
+			.append($('<div class="matrixContainer"></div>').append($('.matrixContainer[data-grid-item=1]').clone(true)));
+		
+		$('.tabs a').click(function() {
+			if ($(this).hasClass('allShowLink')) {
+				$('#gridScroll, #gridWrapper').height(1551);
+			} else {
+				$('#gridScroll, #gridWrapper').height(500);
+			}
+		});
+	}
 
 };
