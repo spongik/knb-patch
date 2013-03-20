b447999deba98bbcc8b0580f904770eb19d00dc3
spongik <isa820@mail.ru>
2013-03-20 20:37:47 +0400

    рефакторинг

--- src/scripts/plugins/mentions-insert.js
+++ src/scripts/plugins/mentions-insert.js
@@ -5,28 +5,59 @@ $.knb.plugins.mentionsInsert.run = function () {
 
 	var activeTextareaClass = 'activeMentionsTextarea';
 	
+	var $replyBtn = $('<a>@</a>')
+		.addClass('flow-reply')
+		.attr('title', 'ответить')
+		.attr('href', '#');
+	
 	var bindMentionsInsert = function() {
 		$comments = $('#comments .comment[mentions!=1]');
 		if  ($comments.length > 0) {
+			$comments = $comments.find('>[mentions!=1]').attr('mentions', '1');
 			$comments
-				.find('>[mentions!=1]')
-				.attr('mentions', '1')
-				.find('.userNamesBlock .userName').before('<a href="#" title="ответить" class="flow-reply comments">@</a>')
-				.parent().find('.flow-reply')
-				.click(function (ev) {
-					$user = $(this).parent().find('.userName');
-					
-					name = $user.html().replace(/<span>.+<\/span>/, '');
-					id = $user.attr('href').replace('/accounts/', '').replace('/', '');
-					
-					$('.' + activeTextareaClass).removeClass(activeTextareaClass);
-					$('.writeComment textarea').addClass(activeTextareaClass);
-					
-					$.knb.fn.mentionInsert(name, id);
-					
-					ev.stopPropagation();
-					ev.preventDefault();
-					return false;
+				.find('.userNamesBlock .userName')
+				.before($replyBtn
+					.clone()
+					.addClass('comments')
+					.click(function (ev) {
+						$user = $(this).parent().find('.userName');
+						
+						name = $user.html();
+						id = $user.attr('href').replace('/accounts/', '').replace('/', '');
+						
+						$('.' + activeTextareaClass).removeClass(activeTextareaClass);
+						$('.writeComment textarea').addClass(activeTextareaClass);
+						
+						$.knb.fn.mentionInsert(name, id);
+						
+						ev.stopPropagation();
+						ev.preventDefault();
+						return false;
+					}));
+			$comments
+				.find('.userNamesBlock .whom')
+				.before($replyBtn
+					.clone()
+					.addClass('comments')
+					.click(function (ev) {
+						$user = $(this).parent().find('.whom');
+						
+						name = $user.html();
+						id = $user.attr('href').replace('/accounts/', '').replace('/', '');
+						
+						$('.' + activeTextareaClass).removeClass(activeTextareaClass);
+						$('.writeComment textarea').addClass(activeTextareaClass);
+						
+						$.knb.fn.mentionInsert(name, id);
+						
+						ev.stopPropagation();
+						ev.preventDefault();
+						return false;
+					}))
+				.find('span')
+				.each(function (i, span) {
+					$span = $(span);
+					$span.insertAfter($span.parent().parent().find('.userName'));
 				});
 		}
 		return true;
@@ -42,13 +73,15 @@ $.knb.plugins.mentionsInsert.run = function () {
 			$shouts = $('> li > .shout[mentions!=1]', $(this))
 				.attr('mentions', '1')
 				.each(function (i, shout) {
+					$shout = $(shout);
 					$(shout).parent().watch(function() {
-						$('.shoutTxt').css('padding-top','0px');
-						$('.shoutAnswers .shout[mentions!=1]', $(this))
-							.attr('mentions', '1')
-							.find('.ava .userName').css('left','70px')
-							.parent().parent().find('.shoutTxt').before('<a href="#" title="ответить" class="flow-reply">@</a>')
-							.parent().find('.flow-reply').click(function (ev) {
+						$answers = $('.shoutAnswers .shout[mentions!=1]', $(this)).attr('mentions', '1');
+						$answers.find('.ava .userName').css('left','70px');
+						$answers.find('.shoutTxt')
+							.css('padding-top','0px')
+							.before($replyBtn
+							.clone()
+							.click(function (ev) {
 								$user = $(this).parent().find('.ava');								
 								name = $user.find('.userName').html();
 								id = $user.attr('href').replace('/accounts/', '').replace('/', '');
@@ -61,27 +94,28 @@ $.knb.plugins.mentionsInsert.run = function () {
 								ev.stopPropagation();
 								ev.preventDefault();
 								return false;
-							});
+							}));
 						return true;
 					}).trigger('DOMSubtreeModified');
-					$(this).find('.userName').css('left','70px');
-					$(this).find('.shoutTxt').before('<a href="#" title="ответить" class="flow-reply">@</a>');
-				})
-				.find('>.flow-reply')
-				.click(function (ev) {
-					$user = $(this).parent().find('.ava');
 					
-					name = $user.find('.userName').html();
-					id = $user.attr('href').replace('/accounts/', '').replace('/', '');
-					
-					$('.' + activeTextareaClass).removeClass(activeTextareaClass);
-					$user.closest('li').find('.answersField textarea').addClass(activeTextareaClass);
-					
-					$.knb.fn.mentionInsert(name, id);
-					
-					ev.stopPropagation();
-					ev.preventDefault();
-					return false;
+					$shout.find('.userName').css('left','70px');
+					$shout.find('.shoutTxt').before($replyBtn
+						.clone()
+						.click(function (ev) {
+							$user = $(this).parent().find('.ava');
+							
+							name = $user.find('.userName').html();
+							id = $user.attr('href').replace('/accounts/', '').replace('/', '');
+							
+							$('.' + activeTextareaClass).removeClass(activeTextareaClass);
+							$user.closest('li').find('.answersField textarea').addClass(activeTextareaClass);
+							
+							$.knb.fn.mentionInsert(name, id);
+							
+							ev.stopPropagation();
+							ev.preventDefault();
+							return false;
+						}));
 				});
 			return true;
 		}).trigger('DOMSubtreeModified');
