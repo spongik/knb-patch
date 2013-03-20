13b6defdbe3447fba4834e2251bc13b9612e0169
eden <edencore@gmail.com>
2013-03-20 14:25:22 +0400

    Всплывающее сообщение

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -13,11 +13,25 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 update = function () {
 	$.ajax({url: KANOBU_URL + NOTIFS_URL, async: true, success: function (data) {
 		var newCount = $(".new").length;
-		if (newCount > 0)
+		if (newCount > 0) {
 			chrome.browserAction.setBadgeText({text: newCount.toString()});
+			popupMsg("Kanobu Flow", "Новые непрочитанные оповещения : "+newCount);
+		}
 		else
 			chrome.browserAction.setBadgeText({text: ""});
 	}});
 }
+
+popupMsg = function (head, message) {
+	var note = webkitNotifications.createNotification(
+		"images/icons/icon48.png",
+		head,
+		message
+	);
+	note.onclick = function () {
+		note.cancel();
+	};
+	note.show();
+};
 
 setInterval(update, INTERVAL);
