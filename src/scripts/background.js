6cee4fa4716d17510353508af707ac79f0336f19
eden <edencore@gmail.com>
2013-03-20 11:13:08 +0400

    Обновление количества новых оповещений

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,3 +1,7 @@
+var KANOBU_URL = "http://kanobu.ru";
+var NOTIFS_URL = "/notifs";
+var INTERVAL = 1000 * 60;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (changeInfo.status === 'loading' && tab.url.indexOf("http://kanobu.ru/") == 0) {
 		chrome.tabs.executeScript(tabId, {
@@ -5,3 +9,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		});
 	}
 });
+
+update = function () {
+	$.ajax({url: KANOBU_URL + NOTIFS_URL, async: true, success: function (data) {
+		var newCount = $(".new").length;
+		chrome.browserAction.setBadgeText({text: newCount});
+	}});
+}
+
+setInterval(update, interval);
