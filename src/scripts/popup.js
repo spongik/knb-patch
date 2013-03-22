72e7dbd684972ef14e80b69c85d233a8d2f9f6b6
eden <edencore@gmail.com>
2013-03-22 11:49:57 +0400

    Открытие всех ссылок в новом окне в popup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -4,7 +4,7 @@ var NOTIFS_URL = "/notifs";
 getNotifs = function () {
 	$.ajax({url: KANOBU_URL + NOTIFS_URL, async: true, success: function (data) {
 		$notifs = $("li:lt(10)", data);
-		$("a", $notifs).attr("target", "_blank").attr("href", function (i, url){
+		$("a", $notifs).attr("href", function (i, url){
 			return KANOBU_URL + url;
 		});
 		$("#content").html($notifs);
