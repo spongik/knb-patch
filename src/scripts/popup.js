e24b2f4dab9476a565a9a514decdc57fc9c48393
eden <edencore@gmail.com>
2013-03-22 13:39:45 +0400

    Открытие ссылок через chrome.tabs.create

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,11 +1,21 @@
 var KANOBU_URL = "http://kanobu.ru";
 var NOTIFS_URL = "/notifs";
 
+init = function () {
+	$("#help").click(function () {
+		chrome.tabs.create({url: $(this).attr('href')});
+		return false;
+	});
+}
+
 getNotifs = function () {
 	$.ajax({url: KANOBU_URL + NOTIFS_URL, async: true, success: function (data) {
 		$notifs = $("li:lt(10)", data);
-		$("a", $notifs).attr("href", function (i, url){
-			return KANOBU_URL + url;
+		$("a", $notifs).each(function (index, link) {
+			$(link).click(function() {
+				chrome.tabs.create({url: link.href});
+				return false;
+			});
 		});
 		$("#content").html($notifs);
 	}});
