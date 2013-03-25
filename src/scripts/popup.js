26897c9e6f53b6e4abced45df4f2c5e04ca91710
eden <edencore@gmail.com>
2013-03-25 09:41:48 +0400

    Обновление оповещений по нажатию на иконку и анимация загрузки

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,12 +1,14 @@
 var KANOBU_URL = "http://kanobu.ru";
 var NOTIFS_URL = "/notifs";
 
-init = function () {
-	$("#help").click(function () {
-		chrome.tabs.create({url: $(this).attr('href')});
+document.addEventListener('DOMContentLoaded', function () {
+	$("#notifs").click(function () {
+		load(getNotifs);
 		return false;
 	});
-}
+	load(getNotifs);
+})
+
 
 getNotifs = function () {
 	$.ajax({url: KANOBU_URL + NOTIFS_URL, async: true, success: function (data) {
@@ -19,6 +21,12 @@ getNotifs = function () {
 		});
 		$("#content").html($notifs);
 	}});
-}
+};
 
-getNotifs();
+load = function (method) {
+	var img = document.createElement('img');
+	img.src = chrome.extension.getURL("/images/loader.gif");
+	img.id = 'loader';
+	$("#content").html(img);
+	method();
+};
