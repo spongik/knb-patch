5f4e98e4c8a9af91151b2143844631aa5473770b
orels <orel-s1@yandex.ru>
2013-12-22 21:21:38 +0300

    Update 5.0.5.3 - fix for usefull-blocks bg + added "to Pub" button

--- src/scripts/plugins/scroll-top.js
+++ src/scripts/plugins/scroll-top.js
@@ -3,8 +3,11 @@ $.knb.plugins.scrollTop = {};
 $.knb.plugins.scrollTop.async = true;
 $.knb.plugins.scrollTop.run = function (){
 	$('body').append('<a href="#" id="scroll-top-btn"><span>&uarr;</span>наверх</a>');
-	if(window.location.pathname == '/'){$('body').append('<a href="#" id="scroll-video-btn">К видео</a>')};
-	if(window.location.pathname == '/'){$('body').append('<a href="#" id="scroll-article-btn">К статьям</a>')};
+	if(window.location.pathname == '/'){
+        $('body').append('<a href="#" id="scroll-video-btn">К видео</a>');
+        $('body').append('<a href="#" id="scroll-article-btn">К статьям</a>');
+        $('body').append('<a href="#" id="scroll-pub-btn">К Пабу</a>');
+    }
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('#scroll-top-btn').css('opacity', '0.4');
@@ -24,9 +27,15 @@ $.knb.plugins.scrollTop.run = function (){
 		return false;
 	});
 	$('#scroll-article-btn').click(function(){
-		var targetScroll = $('.page-content-grid:eq(2)').offset();
-		targetScroll = targetScroll['top'] - 40;
-		$("html, body").animate({ scrollTop: targetScroll }, 600);
+        var targetScroll = $('.usefull-blocks').offset();
+        targetScroll = targetScroll['top'] - 40;
+        $("html, body").animate({ scrollTop: targetScroll }, 600);
 		return false;
 	});
+    $('#scroll-pub-btn').click(function(){
+        var targetScroll = $('.page-content-grid:eq(2)').offset();
+        targetScroll = targetScroll['top'] - 150;
+        $("html, body").animate({ scrollTop: targetScroll }, 600);
+        return false;
+    });
 };
